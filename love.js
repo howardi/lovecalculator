7197a6563a6bc6e0925501ad253b942c86314c90
howardi <wakzclin@gmail.com>
2022-04-24 22:01:03 +0100

    lovecal

---
+++ love.js
@@ -0,0 +1,8 @@
+ var fname = prompt("input your name");
+ var lname =  prompt("input your their name");
+
+ var loveScore = Math.random() * 100;
+  loveScore = Math.floor(loveScore) + 1;
+ if(loveScore > 70){
+     alert("Your loveScore is " + loveScore + "%. You both love yourselves.");
+ }
