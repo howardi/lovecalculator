ca91e0c7f3e6732697fbb01cc92807e88500a32f
howardi <wakzclin@gmail.com>
2022-04-25 09:59:19 +0100

    lovecalc

--- love.js
+++ love.js
@@ -1,8 +1,20 @@
- var fname = prompt("input your name");
- var lname =  prompt("input your their name");
+//  var fname = prompt("input your name");
+//  var lname =  prompt("input your their name");
 
- var loveScore = Math.random() * 100;
-  loveScore = Math.floor(loveScore) + 1;
- if(loveScore > 70){
-     alert("Your loveScore is " + loveScore + "%. You both love yourselves.");
- }
+//  var loveScore = Math.random() * 100;
+//   loveScore = Math.floor(loveScore) + 1;
+//  if(loveScore > 75){
+//      alert("Your loveScore is " + loveScore + "%. You both love yourselves.");
+//  }
+
+ var lovecalcu = document.getElementById("lovecalcu");
+
+function lovecalc(){
+var fname = prompt("input your name");
+ var lname =  prompt("input your spouse name");
+ var loveScores = Math.floor(Math.random() * 100) + 1;
+ var output = fname + " " + "and " + lname  + " are " + loveScores + "%" + " match";
+ return output;
+}
+var lovecalculator = lovecalc();
+lovecalcu.innerHTML = lovecalculator;
